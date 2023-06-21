72dc366da9a80f374ee99186eba26c1c0fcd26fe
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-06-21 13:57:24 -0300

    Update Mission.jsx component to render data

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions, joinMission, leaveMission } from '../redux/missions/missionsSlice';
 
-const Missions = () => (
-  <div>Missions starts here</div>
-);
+const MissionList = () => {
+  const missions = useSelector((state) => state.missions.missions);
+  const loading = useSelector((state) => state.missions.loading);
+  const dispatch = useDispatch();
 
-export default Missions;
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(fetchMissions());
+    }
+  }, [dispatch, missions]);
+
+  const handleJoinMission = (missionId) => {
+    dispatch(joinMission({ missionId }));
+  };
+
+  const handleLeaveMission = (missionId) => {
+    dispatch(leaveMission({ missionId }));
+  };
+
+  if (loading === 'idle') {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="missionsCont" data-testid="mission-item">
+      <table className="table table-bordered table-striped">
+        <thead className="tableHead">
+          <tr>
+            <th className="mission">Mission</th>
+            <th className="desc">Description</th>
+            <th className="status">Status</th>
+          </tr>
+        </thead>
+        <tbody className="tableBody">
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td>
+                <div className={`member ${mission.reserved ? 'activeMem' : 'not-member'}`}>
+                  {mission.reserved ? 'Active Member' : 'NOT A MEMBER'}
+                </div>
+              </td>
+              <td>
+                {mission.reserved ? (
+                  <button type="button" className="button-leave btn btn-danger" onClick={() => handleLeaveMission(mission.mission_id)}>
+                    Leave Mission
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="joinM btn btn-primary"
+                    onClick={() => handleJoinMission(mission.mission_id)}
+                  >
+                    Join Mission
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default MissionList;
